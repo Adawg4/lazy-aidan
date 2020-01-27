731804be494bce3b1c798239642b6862514305d6
Aidan <27599708+Adawg4@users.noreply.github.com>
2020-01-27 08:55:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='lazy-aidan',
+    name='lazy_aidan',
     version='0.0.1',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
