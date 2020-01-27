7b48664e07cd3b9f927dd45c2e8fb32c2f4a57b8
Adawg4 <27599708+Adawg4@users.noreply.github.com>
2020-01-26 21:50:19 -0500

    remove pre readme

--- lazy-aidan.py
+++ lazy-aidan.py
@@ -28,31 +28,3 @@ class Aidan:
 		server.starttls()
 		server.login(self.email,self.pwd)
 		server.sendmail(self.email, self.to,msg)
-'''
-How to use:
-Create a burner gmail account: 
-Go to this and fill out the details.
-https://accounts.google.com/SignUp
-
-Allow account access to the script.
-Go to Google's Account Security Settings: www.google.com/settings/security
-Find the field "Access for less secure apps". Set it to "Allowed".
-
-Add this before the send function (preferably at the top) and initialize with your info
-varname = Aidan(email="sender gmail adress", pwd="your password", to="to email/sms number", carrier="choose from the carrier list below")
-
-Add this with a message with relevant info from your code
-varname.send(msg="New message here")
-
-Carrier list: (Paste the exact string into the the carrier="" in the Aidan() class)
-All Tell
-AT&T
-Rogers
-Sprint
-T-Mobile
-Telus
-Verizon
-Virgin Mobile
-Orange
-'''
-
